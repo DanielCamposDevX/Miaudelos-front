ecb3320d4f261b45cd167ecb5b1d0d45a3e430b8
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-08-11 11:40:45 -0300

    change/Login Screen Design Change

--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import Logo from '../assets/Logo.png'
+import Name from '../assets/LogoName.png'
 import axios from 'axios'
 import { useState } from 'react';
 import { LoadingButton } from '@mui/lab';
 import { TextField, CircularProgress } from '@mui/material';
+
 
 
 export default function Login() {
@@ -30,38 +31,26 @@ export default function Login() {
             })
     }
 
-    
+
 
     return (
         <form onSubmit={handleSubmit} style={{ width: '100%', height: '100vh' }}>
             <Page>
-                <Fade>
-                    <CusImg src={Logo} />
-                    <Container>
-                        <Search variant="filled" type='email' label='Email' color='secondary' required value={email} onChange={(e) => setEmail(e.target.value)} />
-                        <Search variant="filled" type='password' label='Senha' color='secondary' required value={password} onChange={(e) => setPassword(e.target.value)} />
+                <Container >
+                    <CusImg src={Name} />
+                    <Search variant="filled" type='email' label='Email' color='secondary' required value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <Search variant="filled" type='password' label='Senha' color='secondary' required value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <Container style={{ background: 'transparent', height: 'auto', border: 'none' }}>
+                        <Sbutton variant="contained" loading={loading} style={{ fontWeight: '700', fontFamily: 'Lexend Deca', textTransform: 'none', backgroundColor:'#d919ff' }} type='submit'>Entrar</Sbutton>
+                        <Sbutton variant="contained" style={{ backgroundColor: 'orange', fontWeight: '700', marginTop: '20px', fontFamily: 'Lexend Deca', textTransform: 'none' }} onClick={() => { navigate('/cadastro') }}>Cadastre-se</Sbutton>
                     </Container>
-                    <Container>
-                        <Sbutton variant="contained"   loading={loading} style={{ backgroundColor: 'purple',fontWeight: '700',  fontFamily: 'Lexend Deca',textTransform: 'none'}} type='submit'>Entrar</Sbutton>
-                        <Sbutton variant="contained"  loading={loading} style={{ backgroundColor: 'orange', marginTop: '20px',fontWeight: '700',  fontFamily: 'Lexend Deca',textTransform: 'none'}} onClick={() => { navigate('/cadastro') }}>Cadastre-se</Sbutton>
-                    </Container>
-                </Fade>
+                </Container>
             </Page>
         </form>
     );
 }
 
-const Fade = styled.div`
-    width:100%;
-    height:100%;
-    background: rgb(0,0,0);
-    background: linear-gradient(0deg, rgba(0,0,0,0.13209033613445376) 0%, rgba(0,0,0,0.13209033613445376) 100%);
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    justify-content:space-around;
-    font-family: 'Lexend Deca', sans-serif;
-`
+
 const Page = styled.div`
     width: 100%;
     height: 100vh;
@@ -70,37 +59,43 @@ const Page = styled.div`
     align-items:center;
     justify-content:space-around;
     font-family: 'Lexend Deca', sans-serif;
-    background-image: url('https://i.pinimg.com/564x/51/48/9d/51489d4a8d7bd9eefd33d9056a767455.jpg');
+    background-image: url('https://img.freepik.com/fotos-gratis/silhuetas-de-colinas-e-postes-de-luz-sob-um-ceu-nublado-durante-um-lindo-por-do-sol_181624-28611.jpg?w=740&t=st=1691761304~exp=1691761904~hmac=a3f89896fea70bedf01f3aef6c477890dee9b0335c16477789a186d38cdb16a3');
+    background-size: cover;
 `
 
 const CusImg = styled.img`
-    height: 20vh;
-    border-radius: 12px;
-    border: 1px solid rgba(0,0,0,0.1);
-    box-shadow: 0px 4px 24px 0px #383d3499;
-    padding-top:5px;
-    padding-bottom:5px;
-    background-color:white;
-    @media (max-width: 500px) {
-        height: 15vh; 
-    }
+    width: 80%;
+    max-width: 300px;
+    height: auto;
+    border: 1px solid black;
+    background-color: #ffffffdf;
+    border-radius: 15px;
+
 `
 
 const Search = styled(TextField)({
     backgroundColor: '#ffffffe4',
-    fontFamily: 'inherit'
+    fontFamily: 'inherit',
+    width: '80%',
+    height: 'auto',
+    maxWidth: '300px'
 })
 
 const Sbutton = styled(LoadingButton)({
-    fontWeight: '700', 
+    fontWeight: '700',
     fontFamily: 'Lexend Deca',
-    textTransform: 'none'
+    textTransform: 'none',
 })
 
 const Container = styled.div`
+    border: 1px solid black;
+    border-radius: 16px;
     flex-direction:column;
     display:flex;
-    width: 100%;
+    height: 70vh;
+    width: 90%;
+    max-width: 400px;
     align-items: center;
-    justify-content: center;
+    justify-content: space-around;
+    background-image: url('https://i.pinimg.com/564x/51/48/9d/51489d4a8d7bd9eefd33d9056a767455.jpg')
 `
